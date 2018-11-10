de3013fe0b40e416488a004fa9cb2d5fdeade594
Olha Holysh <32979019+pinkmadkitten@users.noreply.github.com>
2018-11-10 22:51:13 +0200

    feat: add deploy to Heroku step, add DataController (#6)

---
+++ src/js/data-controller.js
@@ -0,0 +1,38 @@
+/* eslint no-console: 0 */
+class DataController {
+  constructor (serverPath) {
+    this.server = serverPath;
+  }
+
+  async getAwailableBreeds () {
+    try {
+      let response = await fetch(`${this.server}/breeds`);
+      response = await response.json();
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getAllFacts () {
+    try {
+      let response = await fetch(`${this.server}/facts`);
+      response = await response.json();
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFactAbout (breed) {
+    try {
+      let response = await fetch(`${this.server}/facts/${breed}`);
+      response = await response.json();
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = DataController;
--- src/js/sum.spec.js
+++ src/js/sum.spec.js
@@ -3,3 +3,8 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('sum 1 + 2 to not equal 3', () => {
+  expect(sum('1', 2)).toBe('12');
+  expect(sum('1', 2)).not.toBe(3);
+});
